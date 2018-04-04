4dd117102ec07e23ae52295c047a934113013823
Ynoa Pedro <pedromota99@gmail.com>
2018-04-04 10:46:02 -0300

    Adding radix.py

---
+++ radix.py
@@ -0,0 +1,35 @@
+import math
+
+def create_bucket(base):
+    buckets = []
+    for i in range (base):
+        buckets.append([])
+        
+    return buckets
+
+def sort(items, base):
+    tmp = -1
+    j = 0
+    
+    m = int(math.log(items[0], base)) + 1
+    
+    while j < m:
+    
+        buckets = create_bucket(base)
+        
+        for number in items:
+    
+            tmp = number / math.pow(base, j)
+            digit = int (tmp % base)
+            buckets[digit].append(number)
+
+        print(buckets)
+
+        a = 0 
+        for bucket in buckets:
+            for i in bucket:
+                items[a] = i
+                a += 1
+
+        
+        j = j + 1
